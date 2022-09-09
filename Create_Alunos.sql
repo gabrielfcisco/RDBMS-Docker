700af68c87d55685cecc9478b65c019367c00c60
Gabriel Sousa <gabrifran017@gmail.com>
2022-09-09 16:02:24 -0300

    all sql in one script

--- Create_Alunos.sql
+++ Create_Alunos.sql
@@ -8,3 +8,26 @@ CREATE TABLE alunos (
 
 INSERT INTO alunos (primeiro_nome, sobrenome, RA)
 VALUES ('Gabriel', 'Sousa', '22886279');
+
+CREATE TABLE professores (
+    primeiro_nome varchar(25),
+    sobrenome varchar(25),
+    RP char(8)
+);
+
+INSERT INTO professores (primeiro_nome, sobrenome, RP)
+VALUES ('Leandro', 'Xastre', '00000001'),
+       ('Jair', 'Bolsonaro', '00000022'),
+       ('Luis Inácio', 'Lula da Silva', '00000013'),
+       ('Ciro', 'Gomes', '00000012');
+
+CREATE TABLE materias (
+    creditos int,
+    nome varchar(50),
+    ID char(3)
+);
+
+INSERT INTO materias (nome, ID, creditos)
+VALUES ('Programacao de Computadores', '001', '4'),
+       ('Cálculo', '002', '6'),
+       ('Robótica Computacional', '003', '4');
