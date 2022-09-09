c92a0bad7c7c957a36bf365120dcedebbee2c71a
Gabriel Sousa <gabrifran017@gmail.com>
2022-09-09 12:59:10 -0300

    Create Tables

---
+++ Create_Alunos.sql
@@ -0,0 +1,10 @@
+USE Xastre;
+
+CREATE TABLE alunos (
+    primeiro_nome varchar(25),
+    sobrenome varchar(25),
+    RA char(8)
+);
+
+INSERT INTO alunos (primeiro_nome, sobrenome, RA)
+VALUES ('Gabriel', 'Sousa', '22886279');
---
+++ Create_Professores.sql
@@ -0,0 +1,13 @@
+USE Xastre;
+
+CREATE TABLE professores (
+    primeiro_nomep varchar(25),
+    sobrenomep varchar(25),
+    RP char(8)
+);
+
+INSERT INTO professores (primeiro_nomep, sobrenomep, RP)
+VALUES ('Leandro', 'Xastre', '00000001'),
+       ('Jair', 'Bolsonaro', '00000022'),
+       ('Luis Inácio', 'Lula da Silva', '00000013')
+       ('Ciro', 'Gomes', '00000012');
