b16c72993d82cea4218a0bd91b9ba44479151a93
VITOR YUZO TAKEI <86261092+vytakei@users.noreply.github.com>
2022-09-12 17:37:16 -0300

    Versão Final

--- Create_Tables.sql
+++ Create_Tables.sql
@@ -7,8 +7,9 @@ CREATE TABLE alunos (
 );
 
 INSERT INTO alunos (primeiro_nome, sobrenome, RA)
-VALUES ('Gabriel', 'Sousa', '22886279');
-
+VALUES ('Gabriel', 'Sousa', '22886279'),
+       ('Vitor', 'Takei', '22023740');
+       
 CREATE TABLE professores (
     primeiro_nome varchar(25),
     sobrenome varchar(25),
@@ -40,4 +41,5 @@ CREATE TABLE rel_al_prof_mat (
 );
 
 INSERT INTO rel_al_prof_mat (RA, RP, ID)
-VALUES ('22886279', '0000001', '004');
+VALUES ('22886279', '0000001', '004'),
+       ('22023740', '0000012', '002');
